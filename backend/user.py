543a60e566c5ec0185078b601ba7dac3bd7a1a5d
rafaelbortolatto <rafaelbortolatto@tamu.edu>
2019-03-01 14:18:47 -0600

    Fix for user table

--- backend/user.py
+++ backend/user.py
@@ -3,11 +3,15 @@
 import database
 
 class User():
-	def __init__(self, email, name, pwd, type_id):
+	def __init__(self, email, name, pwd, type_id, user_id=None):
+		self.user_id = user_id		
 		self.email = email
 		self.name = name
 		self.pwd = pwd
 		self.type_id = type_id
+
+	def get_user_id(self):
+		return self.user_id
 
 	def get_email(self):
 		return self.email
@@ -25,38 +29,37 @@ class UserDB(database.Database):
 	def __init__(self, filename):
 		super().__init__(filename)
 
-	def find_user_name(self, user):
+	def __del__(self):
+		super().__del__()
+
+	def find_user_email(self, email):
 		cur = self.db_conn.cursor()
-		cur.execute("SELECT email, name, pwd, type_id FROM user WHERE email=?", (user,))
+		cur.execute("SELECT email, name, pwd, type_id, id FROM user WHERE email=?", (email,))
 		rows = cur.fetchall()
 
 		if not rows:
 			return None
 		else:
-			user = User(rows[0][0], rows[0][1], rows[0][2], rows[0][3])
-			return user 
+			user = User(rows[0][0], rows[0][1], rows[0][2], rows[0][3], rows[0][4])
+			return user
+
+	def create_user(self, user):
+		param = (user.get_email(), user.get_name(), user.get_pwd(), user.get_type_id())
+		sql = '''INSERT INTO user (email, name, pwd, type_id) values (?,?,?,?)'''
+
+		with self.db_conn:
+			cur = self.db_conn.cursor()
+			cur.execute(sql, param)
+			return cur.lastrowid
 
 
+""" Test 
+if __name__ == '__main__':
+	db = UserDB("cscw.db")
+	user = User("rafa@rafa.com", "Rafa", "pass", 1)
 
+	print (db.create_user(user))
+	print (db.find_user_email("rafa@rafa.com").get_pwd())
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	del db
+"""
