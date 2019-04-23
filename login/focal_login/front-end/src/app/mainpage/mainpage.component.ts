11c67f3d9921124f09e2965a07aa050ffcc062e5
JoshuaHowell <howjosh@gmail.com>
2019-04-23 16:22:26 -0500

    JH -- Dashboard -- Convert both courses into JSON objects.

--- login/focal_login/front-end/src/app/mainpage/mainpage.component.ts
+++ login/focal_login/front-end/src/app/mainpage/mainpage.component.ts
@@ -11,34 +11,39 @@ export class MainpageComponent implements OnInit {
   courses = []
 
   update = function(){
-    let cscw = new Course()
-    cscw.name = "Computer Supported Cooperative Work"
-    cscw.modules = []
-
-    let lesson1 = new Module();
-    lesson1.name = "Course 1: Lesson 1";
-    lesson1.storyboard_completed = true;
-    lesson1.filming_complete = true;
-    lesson1.audio_complete = true;
-    lesson1.production_complete = false;
-    lesson1.aggregation_complete = false;
-    lesson1.final_review_complete = false;
-
-    let lesson2 = new Module();
-    lesson2.name = "Course 2: Lesson 2";
-    lesson2.storyboard_completed = true;
-    lesson2.filming_complete = true;
-    lesson2.audio_complete = false;
-    lesson2.production_complete = false;
-    lesson2.aggregation_complete = false;
-    lesson2.final_review_complete = false;
-
-      cscw.modules = [lesson1, lesson2]
+    // let cscw = new Course()
+    // cscw.name = "Computer Supported Cooperative Work"
+    // cscw.modules = []
+    //
+    // let lesson1 = new Module();
+    // lesson1.name = "Course 1: Lesson 1";
+    // lesson1.storyboard_completed = true;
+    // lesson1.filming_complete = true;
+    // lesson1.audio_complete = true;
+    // lesson1.production_complete = false;
+    // lesson1.aggregation_complete = false;
+    // lesson1.final_review_complete = false;
+    //
+    // let lesson2 = new Module();
+    // lesson2.name = "Course 2: Lesson 2";
+    // lesson2.storyboard_completed = true;
+    // lesson2.filming_complete = true;
+    // lesson2.audio_complete = false;
+    // lesson2.production_complete = false;
+    // lesson2.aggregation_complete = false;
+    // lesson2.final_review_complete = false;
+    //
+    //   cscw.modules = [lesson1, lesson2]
 
     // cscw.modules.push(lesson1);
     // cscw.modules.push(lesson2);
 
+    var json_course_1 = '{"name": "Computer Supported Cooperative Work", "modules": [{"name": "Course 1: Lesson 1", "storyboard_completed": true, "filming_complete": true, "audio_complete": true, "production_complete": false, "aggregation_complete": false, "final_review_complete": false}, {"name": "Course 1: Lesson 2", "storyboard_completed": true, "filming_complete": true, "audio_complete": false, "production_complete": false, "aggregation_complete": false, "final_review_complete": false}]}'
+
+    let cscw = JSON.parse(json_course_1)
     this.courses.push(cscw)
+
+    //this.courses.push(cscw)
 
     // let embodiment = new Course()
     // embodiment.name = "Embodied Interaction"
